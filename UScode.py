a6d59ff09a75d562ba2d5594cbd8a8e0d748a38f
VIPABH <a891983.2@gmail.com>
2025-05-01 02:01:28 +0300

    Update UScode.py

--- UScode.py
+++ UScode.py
@@ -1,35 +1,25 @@
 from telethon import TelegramClient, events
 import asyncio, os
-
-api_id = int(os.getenv('API_ID'))
+api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
-ABH = TelegramClient('session', api_id, api_hash)
-
+ABH = TelegramClient('session', int(api_id), api_hash)
 @ABH.on(events.NewMessage(pattern='خاص'))
 async def save(event):
     uid = event.sender_id
     me = await ABH.get_me()
     r = await event.get_reply_message()
     if not r:
-        try:
-            await event.reply("🤔 يجب أن ترد على رسالة.")
-            await asyncio.sleep(3)
-            await event.delete()
-        except: pass
+        await event.edit('🤔 يجب أن ترد على رسالة.')
+        await asyncio.sleep(3)
+        await event.delete()
         return
     if uid == me.id:
-        try:
-            await ABH.forward_messages(me.id, r)
-            await asyncio.sleep(3)
-            await event.delete()
-        except Exception as e:
-            await event.reply(f"❌ حدث خطأ أثناء التحويل:\n{e}")
+          await r.forward_to(me.id)
+          await asyncio.sleep(3)
+          await event.delete()
     else:
-        await asyncio.sleep(1)
-        await event.delete()
-
+        return
 async def main():
     await ABH.start()
     await ABH.run_until_disconnected()
-
 asyncio.run(main())
