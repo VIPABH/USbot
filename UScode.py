e7e753dde2c251e9ef82f480c22a76a2e9125fa1
VIPABH <a891983.2@gmail.com>
2025-05-01 01:21:43 +0300

    Update UScode.py

--- UScode.py
+++ UScode.py
@@ -1,23 +1,13 @@
 from telethon import TelegramClient, events
 import asyncio, os
-
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
-
-# تأكيد أن القيم موجودة
-if not api_id or not api_hash:
-    raise ValueError("يجب تعيين API_ID و API_HASH كمتغيرات بيئية.")
-
-# إنشاء عميل يوزر بوت
 ABH = TelegramClient('session', int(api_id), api_hash)
-
 @ABH.on(events.NewMessage(pattern='خاص'))
 async def save(event):
     uid = event.sender_id
     me = await ABH.get_me()
     r = await event.get_reply_message()
-
-    # تحقق من وجود رد
     if not r:
         try:
             await event.reply('🤔 يجب أن ترد على رسالة.')
@@ -26,8 +16,6 @@ async def save(event):
         except:
             pass
         return
-
-    # إذا كانت الرسالة من نفسك (يعني تعمل كيوزر بوت)
     if uid == me.id:
         try:
             await ABH.forward_messages(me.id, r)
@@ -39,10 +27,7 @@ async def save(event):
         await asyncio.sleep(1)
         await event.delete()
 
-# تشغيل اليوزر بوت
 async def main():
     await ABH.start()
-    print("✅ تم تسجيل الدخول كـ Userbot.")
     await ABH.run_until_disconnected()
-
 asyncio.run(main())
