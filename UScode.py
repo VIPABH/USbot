48da086f7c4e0f97e84e928a4795739320618e55
VIPABH <a891983.2@gmail.com>
2025-05-01 02:07:45 +0300

    Update UScode.py

--- UScode.py
+++ UScode.py
@@ -2,7 +2,8 @@ from telethon import TelegramClient, events
 import asyncio, os
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
-ABH = TelegramClient('session', int(api_id), api_hash)
+session = 'session'
+ABH = TelegramClient(session, int(api_id), api_hash)
 @ABH.on(events.NewMessage(pattern='خاص'))
 async def save(event):
     uid = event.sender_id
@@ -14,11 +15,21 @@ async def save(event):
         await event.delete()
         return
     if uid == me.id:
-          await r.forward_to(me.id)
-          await asyncio.sleep(3)
           await event.delete()
+          await r.forward_to(me.id)
     else:
         return
+@ABH.on(events.NewMessage('.مسح'))
+async def dele(event):
+    await event.delete()
+    r = await event.get_reply_message()
+    await r.delete()
+@ABH.on(events.NewMessage(pattern='جلسه'))
+async def send_session_info(event):
+    me = await ABH.get_me()
+    session_file = f"{session}.session"
+    await ABH.send_message(me.id, f"📎 اسم جلسة الاتصال (Session): `{session_file}`")
+
 async def main():
     await ABH.start()
     await ABH.run_until_disconnected()
