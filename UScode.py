36d3fe0794ebfb92153c607da129f84149956570
VIPABH <a891983.2@gmail.com>
2025-05-02 14:33:10 +0300

    Update UScode.py

--- UScode.py
+++ UScode.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient, events
 import asyncio
-from موارد import th
+from موارد import th, ABH
 @ok
 @ABH.on(events.NewMessage(pattern=r'^.تثبيت$'))
 async def pin(event):
