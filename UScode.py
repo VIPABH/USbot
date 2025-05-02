7d90b0895b1003f21e5f04701d161aa14f14fb17
VIPABH <a891983.2@gmail.com>
2025-05-02 14:14:09 +0300

    Update UScode.py

--- UScode.py
+++ UScode.py
@@ -1,5 +1,18 @@
+from telethon import TelegramClient, events
 import asyncio, os
-from موارد import *
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+session = 'session'
+ABH = TelegramClient(session, int(api_id), api_hash)
+def ok(func):
+    async def wrapper(event):
+        uid = event.sender_id
+        owner = (await event.ABH.get_me()).id
+        if uid != owner:
+            return
+        await func(event)
+    return wrapper
+@ok
 @ABH.on(events.NewMessage(pattern=r'^.تثبيت$'))
 async def pin(event):
     await event.delete()
@@ -128,3 +141,8 @@ async def word(event):
                 elif isinstance(word, int):
                     if str(word) in msg.text:
                         await msg.delete()
+async def main():
+    await ABH.start()
+    await ABH.run_until_disconnected()
+print("usbot is running ◉")
+asyncio.run(main())
