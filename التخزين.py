188d6517ff5e39adeea5d6ed8f86667763588f6d
VIPABH <a891983.2@gmail.com>
2025-05-03 14:58:29 +0300

    Update التخزين.py

--- التخزين.py
+++ التخزين.py
@@ -1,16 +1,20 @@
 from ABH import ABH, events #type: ignore
-from config import * #type: ignore
+from config import gidvar #type: ignore
 @ABH.on(events.NewMessage)
 async def gidvar_save(event):
+    bot = event.is_bot
+    chat = event.chat_id
+    if bot or chat == gidvar or chat == 777000:
+        return
     text = event.text
     me = await ABH.get_me()
-    # if me.username and f"@{me.username}" in text:
-    cid = await event.get_chat()
-    sender = await event.get_sender()
-    chat_id_str = str(cid.id).replace("-100", "")
-    msg_id = event.id
-    await ABH.send_message(
-        -1001852592581,
+    if me.username and f"@{me.username}" in text:
+          cid = await event.get_chat()
+          sender = await event.get_sender()
+          chat_id_str = str(cid.id).replace("-100", "")
+          msg_id = event.id
+          await ABH.send_message(
+          gidvar,
             f"""
 #التــاكــات
 
