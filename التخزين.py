722af339c37c00e38d261c0af8676391504cf526
VIPABH <a891983.2@gmail.com>
2025-05-03 16:07:20 +0300

    Update التخزين.py

--- التخزين.py
+++ التخزين.py
@@ -93,17 +93,24 @@ async def gidvar_save(event):
     text = event.raw_text
     me = await ABH.get_me()
     replied = await event.get_reply_message()
+
     if (
         (me.username and f"@{me.username}" in text) or
         (str(me.id) in text) or
         (replied and replied.sender_id == me.id)
     ):
+        # تأكد من وجود معلومات عن الدردشة (chat)
         chat = await event.get_chat()
+        if not chat:
+            return  # لا تقم بالمتابعة إذا لم يكن هناك معلومات عن الدردشة
+
         chat_id_str = str(chat.id).replace("-100", "")
         msg_id = event.id
-    await ABH.send_message(
-        int(gidvar),
-        f"""
+
+        # إرسال الرسالة إذا تحقق الشرط
+        await ABH.send_message(
+            int(gidvar),
+            f"""
 #التــاكــات
 
 ⌔┊الكــروب : {chat.title if hasattr(chat, 'title') else 'خاص'}
@@ -113,8 +120,8 @@ async def gidvar_save(event):
 ⌔┊الرســالـه : {text}
 
 ⌔┊رابـط الرسـاله : [link](https://t.me/c/{chat_id_str}/{msg_id})
-""",
-        link_preview=False
-            )
+            """,
+            link_preview=False
+        )
 
 print("التخزين شغال")
