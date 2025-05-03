429bbee7b36640f797daac27ac444e9a47279d97
VIPABH <a891983.2@gmail.com>
2025-05-03 14:33:08 +0300

    Update التخزين.py

--- التخزين.py
+++ التخزين.py
@@ -4,13 +4,13 @@ from config import * #type: ignore
 async def gidvar_save(event):
     text = event.text
     me = await ABH.get_me()
-    if me.username and f"@{me.username}" in text:
-        cid = await event.get_chat()
-        sender = await event.get_sender()
-        chat_id_str = str(cid.id).replace("-100", "")
-        msg_id = event.id
-        await ABH.send_message(
-            gidvar,
+    # if me.username and f"@{me.username}" in text:
+    cid = await event.get_chat()
+    sender = await event.get_sender()
+    chat_id_str = str(cid.id).replace("-100", "")
+    msg_id = event.id
+    await ABH.send_message(
+        gidvar,
             f"""
 #التــاكــات
 
