49dd7c3c4fb5d8f20f1c7d52b438c34b0b5fc43b
VIPABH <a891983.2@gmail.com>
2025-05-03 13:19:03 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from telethon import events
 from UScode import *
-from config import * 
 from ABH import *
-from موارد import * 
+# from config import * 
+# from موارد import * 
 import asyncio
 async def main():
     await ABH.start()
