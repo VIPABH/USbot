4528b66f11c8afce26a1ef72e89a4aaf99c3a4e3
VIPABH <a891983.2@gmail.com>
2025-05-03 13:13:14 +0300

    Update UScode.py

--- UScode.py
+++ UScode.py
@@ -129,3 +129,4 @@ async def word(event):
                 elif isinstance(word, int):
                     if str(word) in msg.text:
                         await msg.delete()
+                        print('UScode is running')
