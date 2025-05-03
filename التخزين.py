bb8c898243970f51149d932e44118a90bc625f24
VIPABH <a891983.2@gmail.com>
2025-05-03 15:34:26 +0300

    Update التخزين.py

--- التخزين.py
+++ التخزين.py
@@ -2,33 +2,35 @@ from ABH import ABH, events #type: ignore
 from config import * #type: ignore
 print(gidvar)
 print(hidvar)
-@ABH.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+@ABH.on(events.NewMessage(incoming=True, func=lambda e: e.is_reply or e.raw_text))
 async def gidvar_save(event):
     sender = await event.get_sender()
-    bot = sender.bot
-    chat = event.chat_id
-    if bot or chat == gidvar or chat == 777000:
+    if sender.bot or event.chat_id in (gidvar, 777000):
         return
-    text = event.text
+    text = event.raw_text
     me = await ABH.get_me()
-    if me.username and f"@{me.username}" in text:
-          cid = await event.get_chat()
-          sender = await event.get_sender()
-          chat_id_str = str(cid.id).replace("-100", "")
-          msg_id = event.id
-          await ABH.send_message(
-          gidvar,
+    replied = await event.get_reply_message()
+    if (
+        (me.username and f"@{me.username}" in text) or
+        (str(me.id) in text) or
+        (replied and replied.sender_id == me.id)
+    ):
+        chat = await event.get_chat()
+        chat_id_str = str(chat.id).replace("-100", "")
+        msg_id = event.id
+        await ABH.send_message(
+            gidvar,
             f"""
 #التــاكــات
 
-⌔┊الكــروب : {cid.title if hasattr(cid, 'title') else 'خاص'}
+⌔┊الكــروب : {chat.title if hasattr(chat, 'title') else 'خاص'}
 
-⌔┊المرسل :  {sender.first_name}
+⌔┊المرسل : {sender.first_name}
 
 ⌔┊الرســالـه : {text}
 
 ⌔┊رابـط الرسـاله : [link](https://t.me/c/{chat_id_str}/{msg_id})
-""",
+            """,
             link_preview=False
         )
 print("التخزين شغال")
