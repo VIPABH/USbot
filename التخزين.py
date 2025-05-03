138c95c683bd728aea00b9ee9f87f0d572ebb654
VIPABH <a891983.2@gmail.com>
2025-05-03 14:30:22 +0300

    Update التخزين.py

--- التخزين.py
+++ التخزين.py
@@ -1,5 +1,5 @@
 from ABH import ABH, events #type: ignore
-from config import gidvar #type: ignore
+from config import * #type: ignore
 @ABH.on(events.NewMessage)
 async def gidvar_save(event):
     text = event.text
