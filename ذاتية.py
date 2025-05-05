91dc1447b500d5144c18a8df185ff26266e272a5
VIPABH <a891983.2@gmail.com>
2025-05-05 14:37:29 +0300

    Update ذاتية.py

--- ذاتية.py
+++ ذاتية.py
@@ -1,7 +1,7 @@
 from ABH import ABH, events  # type: ignore
 from config import *  # type: ignore
 import os
-Aljoker_Asbo3 = {
+ABH_Asbo3 = {
     'Monday': 'الاثنين',
     'Tuesday': 'الثلاثاء',
     'Wednesday': 'الأربعاء',
@@ -13,8 +13,8 @@ Aljoker_Asbo3 = {
 @ABH.on(events.NewMessage(pattern="^جلب$"))
 async def dato(event):
     if not event.is_reply:
-          lMl10l = await event.get_reply_message()
-          pic = await lMl10l.download_media()
+          ABH = await event.get_reply_message()
+          pic = await ABH.download_media()
     await ABH.send_file(
         "me",
         pic,
@@ -27,12 +27,12 @@ async def Hussein(event, caption):
     media = await event.download_media()
     sender = await event.get_sender()
     sender_id = event.sender_id
-    lMl10l_date = event.date.strftime("%Y-%m-%d")
-    lMl10l_day = Aljoker_Asbo3[event.date.strftime("%A")]
+    ABH_date = event.date.strftime("%Y-%m-%d")
+    ABH_day = ABH_Asbo3[event.date.strftime("%A")]
     await ABH.send_file(
         "me",
         media,
-        caption=caption.format(sender.first_name, sender_id, lMl10l_date, lMl10l_day),
+        caption=caption.format(sender.first_name, sender_id, ABH_date, ABH_day),
         parse_mode="markdown"
     )
 def joker_unread_media(message):
@@ -41,17 +41,21 @@ async def Hussein(event, caption):
     media = await event.download_media()
     sender = await event.get_sender()
     sender_id = event.sender_id
-    lMl10l_date = event.date.strftime("%Y-%m-%d")
-    lMl10l_day = Aljoker_Asbo3[event.date.strftime("%A")]
+    ABH_date = event.date.strftime("%Y-%m-%d")
+    ABH_day = ABH_Asbo3[event.date.strftime("%A")]
     await ABH.send_file(
         "me",
         media,
-        caption=caption.format(sender.first_name, sender_id, lMl10l_date, lMl10l_day),
+        caption=caption.format(sender.first_name, sender_id, ABH_date, ABH_day),
         parse_mode="markdown"
     )
     os.remove(media)
 @ABH.on(events.NewMessage(func=lambda e: e.is_private and joker_unread_media(e)))
 async def Reda(event):
+       0 = event.sender.first_name
+       1 = event.sender_id
+       2 = event.date.strftime("%Y-%m-%d")
+       3 = ABH_Asbo3[event.date.strftime("%A")]
        caption = f"""**
        ♡ تم حفظ الذاتية بنجاح ✓
 ♡ أسم المرسل : [{0}](tg://user?id={1})
