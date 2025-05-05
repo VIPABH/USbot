d3f5f191ade6b127c201b9e69bb0cba3aa394a2a
VIPABH <a891983.2@gmail.com>
2025-05-06 00:21:23 +0300

    Update امسح.py

--- امسح.py
+++ امسح.py
@@ -1,23 +1,23 @@
+from ABH import ABH, events, ok #type: ignore
+from التخزين import hidvar #type: ignore
 from telethon.tl.types import (
     InputMessagesFilterDocument,
     InputMessagesFilterPhotos,
     InputMessagesFilterUrl
 )
-from ABH import ABH, events, ok #type: ignore
-from التخزين import hidvar #type: ignore
 @ok
 @ABH.on(events.NewMessage(pattern="^.امسح$"))
 async def delete_all(event):
     await event.delete()
-        filters = {
-            "الملفات": InputMessagesFilterDocument,
-            "الروابط": InputMessagesFilterUrl,
-            "الصور": InputMessagesFilterPhotos
+    filters = {
+        "الملفات": InputMessagesFilterDocument,
+        "الروابط": InputMessagesFilterUrl,
+        "الصور": InputMessagesFilterPhotos
         }
-        total_deleted = 0
-        deleted_counts = {key: 0 for key in filters.keys()}
-        for msg_type, msg_filter in filters.items():
-            async for message in event.client.iter_messages(event.chat_id, filter=msg_filter):
+    total_deleted = 0
+    deleted_counts = {key: 0 for key in filters.keys()}
+    for msg_type, msg_filter in filters.items():
+        async for message in event.client.iter_messages(event.chat_id, filter=msg_filter):
                 excluded_user_ids = [793977288, 1421907917, 7308514832, 6387632922, 7908156943]
                 if message.sender_id in excluded_user_ids:
                     continue 
