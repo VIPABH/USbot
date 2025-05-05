1195258e4a22147e885e2a1a965d8a3aa48ad1bd
VIPABH <a891983.2@gmail.com>
2025-05-05 15:13:55 +0300

    Create ميمز.py

---
+++ ميمز.py
@@ -0,0 +1,12 @@
+from ABH import ABH, ok, events #type:ignore
+import asyncio
+@ok
+@ABH.on(events.NewMessage(pattern='^ها$', outgoing=True))
+async def meme(event):
+    await event.delete()
+    r = await event.get_reply_message()
+    if not r:
+        return
+    url = f"https://t.me/vipabh/1115"
+    await r.ABH.send_file(r.id, url, caption="**Meme**", reply_to=r.id)
+print('meme is running')
