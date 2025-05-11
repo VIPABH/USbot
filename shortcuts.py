12c037084a7620e18c2ea25ed5f99a4aeb1c07f8
VIPABH <a891983.2@gmail.com>
2025-05-12 00:18:45 +0300

    Update shortcuts.py

--- shortcuts.py
+++ shortcuts.py
@@ -20,11 +20,11 @@ class shortcuts:
         self.buttons = event.message.buttons if event.message else None
         self.fwd_from = event.message.fwd_from if event.message else None
         self.entities = event.message.entities if event.message else None
-        self.title = event.chat.title if event.chat.title else event.sender_id
         self.is_reply = bool(event.message.reply_to) if event.message else False
         self.mentions = event.message.get_entities_text() if event.message else None
         self.file = event.message.file if event.message and event.message.media else None
         self.reply_msg_id = event.message.reply_to_msg_id if event.message and event.message.reply_to else None
+        self.title = event.chat.title if not event.is_private and event.chat and hasattr(event.chat, 'title') else event.sender.first_name or str(event.sender_id)
         self.link = f"https://t.me/c/{str(event.chat_id)[4:]}/{event.id}" if getattr(event, 'is_group', False) and str(event.chat_id).startswith("-100") else None
 async def hint(text):
     await ABH.send_message(int(HVAR), text)
