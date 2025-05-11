da34efe7c68ab280b36b16b347fdf9523e3aa191
VIPABH <a891983.2@gmail.com>
2025-05-12 00:05:26 +0300

    Update shortcuts.py

--- shortcuts.py
+++ shortcuts.py
@@ -16,7 +16,6 @@ class shortcuts:
         self.media = event.message.media if event.message else None
         self.name = event.sender.first_name if event.sender else '.'
         self.buttons = event.message.buttons if event.message else None
-        self.link = f"https://t.me/c/{str(event.chat_id)[4:]}/{event.id}"
         self.fwd_from = event.message.fwd_from if event.message else None
         self.entities = event.message.entities if event.message else None
         self.title = event.chat.title if event.chat.title else event.sender_id
@@ -24,5 +23,6 @@ class shortcuts:
         self.mentions = event.message.get_entities_text() if event.message else None
         self.file = event.message.file if event.message and event.message.media else None
         self.reply_msg_id = event.message.reply_to_msg_id if event.message and event.message.reply_to else None
+        self.link = None if event.is_private else f"https://t.me/c/{str(event.chat_id)[4:]}/{event.id}"
 async def hint(self, text):
     await self.send_message(int(HVAR), text)
