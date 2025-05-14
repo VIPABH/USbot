98c16785e11089ada11535499245922bcd32294d
VIPABH <a891983.2@gmail.com>
2025-05-14 21:01:43 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import asyncio
 async def main():
     await config_vars(events)
     await ABH.start()
-    await config_vars(event)
     print("run is running")
+    await ABH.run_until_disconnected()
 from config import *
 from UScode import *
 from التخزين import *
@@ -13,5 +13,4 @@ from ذاتية import *
 from ميمز import *
 from امسح import *
 from الردود import *
-await ABH.run_until_disconnected()
 asyncio.run(main())
