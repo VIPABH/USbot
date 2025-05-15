133f07f4b171db6f2bf441d5ce616a481875bb4e
VIPABH <a891983.2@gmail.com>
2025-05-16 01:45:12 +0300

    Update run.py

--- run.py
+++ run.py
@@ -13,7 +13,6 @@ from امسح import *
 from الردود import *
 async def main():
  print("✅ تم تشغيل البوت بنجاح.")
- if not await ABH.is_user_authorized():
   print("❌ البوت غير مفعل أو التوكن غير صحيح.")
   return
  await config_vars(events)
