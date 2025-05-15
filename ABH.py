7a2c83c79b111133d8b3db17afdf46fae9265621
VIPABH <a891983.2@gmail.com>
2025-05-16 01:54:00 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -2,7 +2,7 @@ from telethon import TelegramClient, events
 import asyncio, os
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
-ABH = TelegramClient('session1', api_id, api_hash)
+ABH = TelegramClient('session', api_id, api_hash)
 def ok(func):
     async def wrapper(event):
         uid = event.sender_id
