5ee4f9f664c88736a116e5f14b20b54e5609bdc4
VIPABH <a891983.2@gmail.com>
2025-05-16 18:48:16 +0300

    Update التخزين.py

--- التخزين.py
+++ التخزين.py
@@ -10,10 +10,10 @@ async def create_group(name, about):
     group = result.chats[0]
     return group.id, group.title
 
-config_file = "var.json"
 @ABH.on(events.NewMessage(pattern='/config'))
 async def config_vars():
     global gidvar, hidvar
+    config_file = "var.json"
     me = await ABH.get_me()
 
     async for msg in ABH.iter_messages(me.id):
