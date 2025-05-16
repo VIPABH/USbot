10e7770cc0f3a38ebba3e2efb962d8e4b5e96ee1
VIPABH <a891983.2@gmail.com>
2025-05-16 19:07:37 +0300

    Update التخزين.py

--- التخزين.py
+++ التخزين.py
@@ -2,7 +2,7 @@ from telethon.tl.functions.channels import CreateChannelRequest
 from ABH import ABH, events  # type: ignore
 from config import *  # type: ignore
 from telethon.tl.types import User
-import re, os
+import re, os, json
 gidvar = None
 hidvar = None
 async def create_group(name, about):
