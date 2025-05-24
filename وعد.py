eb57aa7856f1da71b8c8194920841cc404e27a86
VIPABH <a891983.2@gmail.com>
2025-05-24 10:17:10 +0300

    Update وعد.py

--- وعد.py
+++ وعد.py
@@ -4,18 +4,15 @@ import asyncio
 target_user_id = 1421907917
 @ABH.on(events.NewMessage(pattern=r"^كلمات (\d+)$"))
 async def words(event):
-    if not event.out:
-        return
-    num = int(event.pattern_match.group(1)) or 1
-async with ABH.conversation(event.chat_id, timeout=60) as conv:
-    await conv.send_message("كلمات")
-    try:
-        while True:
-            msg = await conv.get_response()
-            if msg.sender_id != target_user_id:
-                continue
-            text = msg.raw_text.strip()
-            await conv.send_message(f" تم استقبال النص:\n{text}")
-            break
-    except asyncio.TimeoutError:
-        await conv.send_message(" لم يتم الرد خلال المهلة المحددة.")
+    async with ABH.conversation(event.chat_id, timeout=60) as conv:
+        await conv.send_message("كلمات")
+        try:
+            while True:
+                msg = await conv.get_response()
+                if msg.sender_id != target_user_id:
+                    continue
+                text = msg.raw_text.strip()
+                await conv.send_message(f" تم استقبال النص:\n{text}")
+                break
+        except asyncio.TimeoutError:
+            await conv.send_message(" لم يتم الرد خلال المهلة المحددة.")
