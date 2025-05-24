4413ddc9e4fb1b189abb72851f3898ae490e95a9
VIPABH <a891983.2@gmail.com>
2025-05-24 10:22:43 +0300

    Update وعد.py

--- وعد.py
+++ وعد.py
@@ -1,18 +1,24 @@
 from telethon import events
 from ABH import ABH
-import asyncio
+import asyncio, re
 target_user_id = 1421907917
 @ABH.on(events.NewMessage(pattern=r"^كلمات (\d+)$"))
 async def words(event):
-    async with ABH.conversation(event.chat_id, timeout=60) as conv:
-        await conv.send_message("كلمات")
-        try:
-            while True:
-                msg = await conv.get_response()
-                if msg.sender_id != target_user_id:
-                    continue
-                text = msg.raw_text.strip()
-                await conv.send_message(f" تم استقبال النص:\n{text}")
-                break
-        except asyncio.TimeoutError:
-            await conv.send_message(" لم يتم الرد خلال المهلة المحددة.")
+    num = int(event.pattern_match.group(1)) or 1
+    for i in range(num + 1):
+        async with ABH.conversation(event.chat_id, timeout=10) as conv:
+            await conv.send_message("كلمات")
+            try:
+                while True:
+                    msg = await conv.get_response()
+                    if msg.sender_id != target_user_id:
+                        continue
+                    text = msg.raw_text.strip()
+                    text = msg.raw_text.strip()
+                    match = re.search(r"\(\s*(.+?)\s*\)", text)
+                    if match:
+                        text = match.group(1)
+                        await conv.send_message(text)
+                    break
+            except asyncio.TimeoutError:
+                return
