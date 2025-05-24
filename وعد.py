fa9b35b9b16ef866be2e46e0a600fdce247a6547
VIPABH <a891983.2@gmail.com>
2025-05-24 16:28:24 +0300

    Update وعد.py

--- وعد.py
+++ وعد.py
@@ -15,11 +15,32 @@ async def words(event):
                     if msg.sender_id != target_user_id:
                         continue
                     text = msg.raw_text.strip()
-                    text = msg.raw_text.strip()
                     match = re.search(r"\(\s*(.+?)\s*\)", text)
                     if match:
                         text = match.group(1)
                         await conv.send_message(text)
+                    break
+            except asyncio.TimeoutError:
+                return
+target_user_id = 1421907917
+@ABH.on(events.NewMessage(pattern=r"^تركيب (\d+)$", outgoing=True))
+async def words(event):
+    await event.delete()
+    num = int(event.pattern_match.group(1)) or 1
+    for i in range(num):
+        async with ABH.conversation(event.chat_id, timeout=10) as conv:
+            await conv.send_message("تركيب")
+            try:
+                while True:
+                    msg = await conv.get_response()
+                    if msg.sender_id != target_user_id:
+                        continue
+                    text = msg.raw_text.strip()
+                    match = re.search(r"\(\s*(.+?)\s*\)", text)
+                    if match:
+                        text = match.group(1)
+                        merged = ''.join(text.split())
+                        await conv.send_message(merged)
                     break
             except asyncio.TimeoutError:
                 return
