596bd9a1a4e31bc3cb1366dd9c2429a200ef079f
VIPABH <a891983.2@gmail.com>
2025-05-24 10:11:52 +0300

    Update وعد.py

--- وعد.py
+++ وعد.py
@@ -8,7 +8,7 @@ async def words(event):
         return
     num = int(event.pattern_match.group(1)) or 1
     async with ABH.conversation(event.chat_id, timeout=60) as conv:
-        await conv.send_message("✏️ أرسل الكلمات المطلوبة الآن...")
+        await conv.send_message("كلمات")
         try:
             while True:
                 msg = await conv.get_response()
