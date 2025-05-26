b67689e4b9c90989aa21c09a0fcc232d2fb0b4f8
VIPABH <a891983.2@gmail.com>
2025-05-26 03:14:49 +0300

    Update ذاتية.py

--- ذاتية.py
+++ ذاتية.py
@@ -13,7 +13,9 @@ ABH_Asbo3 = {
 @ABH.on(events.NewMessage(pattern=r"^جلب(?: (.+))?$", outgoing=True))
 async def dato(event):
     input_link = event.pattern_match.group(1)
-    x = await ABH.get_me().id
+    me = await ABH.get_me()
+    x = me.id
+
     if input_link:
         try:
             pic = await event.client.download_media(input_link)
@@ -23,19 +25,19 @@ async def dato(event):
             await event.delete()
             return
     elif event.is_reply:
-        ABH = await event.get_reply_message()
-        pic = await ABH.download_media()
+        reply_msg = await event.get_reply_message()
+        pic = await reply_msg.download_media()
     else:
         await event.edit(" يجب الرد على رسالة تحتوي على وسائط أو إرسال رابط مباشر.")
         await asyncio.sleep(3)
         await event.delete()
         return
-    await ABH.client.send_file(
+
+    await ABH.send_file(
         x,
         pic,
         caption="- تـم حفظ الصـورة بنجـاح ✓"
     )
-    await event.delete()
     await event.delete()
 async def Hussein(event, caption):
     media = await event.download_media()
