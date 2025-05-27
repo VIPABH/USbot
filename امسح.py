69e0ebdbaa0be9ac7e6b1fabd4b5056097ab278e
VIPABH <a891983.2@gmail.com>
2025-05-27 14:45:37 +0300

    Update امسح.py

--- امسح.py
+++ امسح.py
@@ -6,8 +6,8 @@ from telethon.tl.types import (
     InputMessagesFilterPhotos,
     InputMessagesFilterUrl
 )
-HVAR = vars("hidvar")
-GVAR = vars("gidvar")
+# HVAR = vars("hidvar")
+# GVAR = vars("gidvar")
 @ABH.on(events.NewMessage(pattern="^.امسح$", outgoing=True))
 async def delete_all(event):
     await event.delete()
@@ -26,14 +26,14 @@ async def delete_all(event):
                     await message.delete()
                     deleted_counts[msg_type] += 1
                     total_deleted += 1
-    if total_deleted > 0:
-        details = "\n".join([f"{msg_type}: {count}" for msg_type, count in deleted_counts.items() if count > 0])
-        message_text = f"🗑️ تم حذف {total_deleted} رسالة.\n\n التفاصيل:\n{details}"
-    else:
-        message_text = "لم يتم العثور على رسائل مطابقة للفلاتر المحددة."
-    try:
-        await ABH.send_message(int(HVAR), message_text)
-    except Exception as e:
-        print("Error:", e)
-        print("HVAR:", HVAR)
-        await ABH.send_message(event.chat_id, "حدث خطأ أثناء إرسال الرسالة إلى مجموعة الإشعارات.")
+    # if total_deleted > 0:
+    #     details = "\n".join([f"{msg_type}: {count}" for msg_type, count in deleted_counts.items() if count > 0])
+    #     message_text = f"🗑️ تم حذف {total_deleted} رسالة.\n\n التفاصيل:\n{details}"
+    # else:
+    #     message_text = "لم يتم العثور على رسائل مطابقة للفلاتر المحددة."
+    # try:
+    #     await ABH.send_message(int(HVAR), message_text)
+    # except Exception as e:
+    #     print("Error:", e)
+    #     print("HVAR:", HVAR)
+    #     await ABH.send_message(event.chat_id, "حدث خطأ أثناء إرسال الرسالة إلى مجموعة الإشعارات.")
