f702740bdc35a218c8e11ab298ecddc2224ac3c3
VIPABH <a891983.2@gmail.com>
2025-05-27 12:50:43 +0300

    Update run.py

--- run.py
+++ run.py
@@ -11,10 +11,7 @@ from ذاتية import *
 from ميمز import *
 from ABH import *
 from وعد import *
-
-
 async def run_cmd(command: str):
-    """تشغيل أمر في الشيل بشكل غير متزامن وانتظار نهايته"""
     process = await asyncio.create_subprocess_shell(
         command,
         stdout=asyncio.subprocess.PIPE,
@@ -22,22 +19,18 @@ async def run_cmd(command: str):
     )
     stdout, stderr = await process.communicate()
     return stdout.decode().strip(), stderr.decode().strip(), process.returncode
-
 @ABH.on(events.NewMessage(pattern="^تحديث$", outgoing=True))
 async def update_repo(event):
-    msg = await event.edit("⏳ جاري جلب آخر التحديثات من الريبو عبر git pull ...")
+    msg = await event.edit(" جاري جلب آخر التحديثات من الريبو عبر...")
     stdout, stderr, code = await run_cmd("git pull")
     if code == 0:
-        await msg.edit(f"✅ تحديث السورس بنجاح:\n\n{stdout}\n\n"
-                       f"⚠️ يرجى إعادة تشغيل البوت يدويًا أو انتظر إعادة التشغيل التلقائية.")
+        await msg.edit(f" تحديث السورس بنجاح")
     else:
-        await msg.edit(f"❌ حدث خطأ أثناء التحديث:\n\n{stderr}")
-
+        await msg.edit(f" حدث خطأ أثناء التحديث:\n\n{stderr}")
 async def main():
     await ABH.start()
     print("البوت شغال الآن")
     await ABH.run_until_disconnected()
-
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
