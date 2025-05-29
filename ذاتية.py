fb401c6b4afada7fc177142114465e9775f3fb2e
VIPABH <a891983.2@gmail.com>
2025-05-29 23:55:43 +0300

    Update ذاتية.py

--- ذاتية.py
+++ ذاتية.py
@@ -12,16 +12,13 @@ async def get(event):
 async def Hussein(event,caption,input_link):
  me=await ABH.get_me()
  x=me.id
- try:
-  media=await event.client.download_media(input_link)
-  sender=await event.get_sender()
-  sender_id=event.sender_id
-  ABH_date=event.date.strftime("%Y-%m-%d")
-  ABH_day=ABH_Asbo3[event.date.strftime("%A")]
-  await ABH.send_file(x,media,caption=caption,parse_mode="markdown")
-  os.remove(media)
- except Exception as e:
-  await event.reply(f"خطأ أثناء تحميل أو إرسال الوسائط:\n{str(e)}")
+ media=await event.client.download_media(input_link)
+ sender=await event.get_sender()
+ sender_id=event.sender_id
+ ABH_date=event.date.strftime("%Y-%m-%d")
+ ABH_day=ABH_Asbo3[event.date.strftime("%A")]
+ await ABH.send_file(x,media,caption=caption,parse_mode="markdown")
+ os.remove(media)
 def joker_unread_media(message):
  return message.media_unread and (message.photo or message.video)
 async def Hussein_event(event,caption):
