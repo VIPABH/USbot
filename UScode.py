3a47d5d8da8d36e60c93223f90ff65e6e05d7e5c
VIPABH <a891983.2@gmail.com>
2025-05-29 11:54:41 +0300

    Update UScode.py

--- UScode.py
+++ UScode.py
@@ -40,7 +40,7 @@ async def save(event):
     else:
         await event.delete()
         await r.forward_to(me.id)
-@ABH.on(events.NewMessage(pattern=r'^.مسح(?: (\d+))?$', outgoing=True))
+@ABH.on(events.NewMessage(pattern=r'^\.مسح(?: (\d{1,3}))?$', outgoing=True))
 async def delet(event):
     num = event.pattern_match.group(1) or 0
     r = await event.get_reply_message()
