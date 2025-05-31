1da073aea3642c7a126bf47a19a73bf8e9b0bf71
VIPABH <a891983.2@gmail.com>
2025-05-31 14:39:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 from telethon import events
-import os, sys, asyncio
 from ABH import ABH
 now = datetime.now()
 تاريخ = now.strftime("%Y-%m-%d")
 ساعة = now.strftime("%I:%M:%S %p")
+وقت_بدء_التشغيل = datetime.now()
 @ABH.on(events.NewMessage(pattern="^.فحص|فحص", outgoing=True))
 async def testup(event):
-    now = datetime.now()
-    التاريخ = now.strftime("%Y-%m-%d")
-    السااعة = now.strftime("%I:%M:%S %p")
+    الآن = datetime.now()
+    مدة_التشغيل = الآن - وقت_بدء_التشغيل
+    التاريخ = الآن.strftime("%Y-%m-%d")
+    الساعة = الآن.strftime("%I:%M:%S %p")
     start = datetime.now()
     await event.edit("**᯽︙ جـاري فـحص الـبـوت**")
     end = datetime.now()
@@ -17,30 +18,11 @@ async def testup(event):
     cap = (
         f"**᯽︙ السورس شغال**\n"
         f"᯽︙ **الـتـاريخ:** `{التاريخ}`\n"
-        f"᯽︙ **الـسـاعه:** `{السااعة}`\n"
+        f"᯽︙ **الـسـاعه:** `{الساعة}`\n"
+        f"᯽︙ **شغال من:** `{مدة_التشغيل}`\n"
         f"᯽︙ **الـزمن:** `{ms} ms`\n"
         f"᯽︙ **الـبـوت شغال 100%**"
     )
     await event.delete()
     pic = 'https://files.catbox.moe/ebn0d8.jpg'
     await event.respond(file=pic, message=cap)
-# async def run_cmd(command: str):
-#     """تشغيل أمر في الشيل بشكل غير متزامن وانتظار نهايته"""
-#     process = await asyncio.create_subprocess_shell(
-#         command,
-#         stdout=asyncio.subprocess.PIPE,
-#         stderr=asyncio.subprocess.PIPE
-#     )
-#     stdout, stderr = await process.communicate()
-#     return stdout.decode().strip(), stderr.decode().strip(), process.returncode
-# @ABH.on(events.NewMessage(pattern="^اعادة تشغيل$", outgoing=True))
-# async def restart_bot(event):
-#     msg = await event.edit("⏳ جاري جلب آخر التحديثات من الريبو عبر git pull ...")
-#     stdout, stderr, code = await run_cmd("git pull")
-#     if code == 0:
-#         await msg.edit(f" تحديث السورس بنجاح:\n\n{stdout}\n\nجاري إعادة الاتصال بالبوت...")
-#         await ABH.disconnect()
-#         await ABH.start()
-#         await msg.edit(" تم إعادة الاتصال بالبوت بنجاح بعد التحديث.")
-#     else:
-#         await msg.edit(f" حدث خطأ أثناء التحديث:\n\n{stderr}")
