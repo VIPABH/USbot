013e0591953951503ad62f387dac6fcbf5ce1326
VIPABH <a891983.2@gmail.com>
2025-06-01 17:10:21 +0300

    Update ⁨الردود.py⁩

--- الردود.py
+++ الردود.py
@@ -2,10 +2,14 @@ from ABH import *
 from telethon import events
 x = ()
 @ABH.on(events.NewMessage(pattern="اضف قناة تفاعل (.+)"))
-async def add_ch(event)
-    ch = event.pattern.match.group(2)
-    if ch.startwith("-100"):
+async def add_ch(event):
+    ch = event.pattern_match.group(1)
+    if ch.startswith("-100"):
         x.add(ch)
+        await event.reply("تم إضافة القناة بنجاح!")
     else:
-        await event.reply("هذا مو ايدي قناة!!")
+        await event.reply("هذا ليس آيدي قناة!!")
         return
+@ABH.on(events.Newmessage(pattern="القنوات"))
+async def show(event)
+    await event.reply(x)
