3f1c7000ebe7e442fd3267150ac2ac5dff6a267d
VIPABH <a891983.2@gmail.com>
2025-06-08 19:32:28 +0300

    Update وعد.py

--- وعد.py
+++ وعد.py
@@ -6,7 +6,7 @@ target_user_id = 1421907917
 async def words(event):
     await event.delete()
     num = int(event.pattern_match.group(1)) or 1
-    time = int(event.pattern_match.group(1)) or 1
+    time = int(event.pattern_match.group(2)) or 1
     for i in range(num):
         async with ABH.conversation(event.chat_id, timeout=10) as conv:
             await conv.send_message("كلمات")
