10b2bf433aadefeae3086024ce8933c535933cae
VIPABH <a891983.2@gmail.com>
2025-07-03 11:53:30 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -1,17 +1,22 @@
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 import asyncio, os, json
-api_id = int(os.getenv("API_ID") or input("📌 أدخل api_id: "))
-api_hash = os.getenv("API_HASH") or input("📌 أدخل api_hash: ")
+api_id = int(os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
 SESSION_FILE = "session.txt"
 if os.path.exists(SESSION_FILE):
     with open(SESSION_FILE, "r") as f:
         session_str = f.read().strip()
-        print("✅ تم العثور على جلسة محفوظة")
 else:
     session_str = None
-    print("⚠️ لا توجد جلسة محفوظة، سيتم إنشاء جلسة جديدة")
 ABH = TelegramClient(StringSession(session_str), api_id, api_hash)
+@ABH.on(events.NewMessage(pattern=r"^(كود الجلسة|/session)$", outgoing=True))
+async def send_session(event):
+    session_string = ABH.session.save()
+    with open(SESSION_FILE, "w") as f:
+        f.write(session_string)
+    await ABH.send_message("me", f" Session String:\n`{session_string}`")
+    await event.edit(" تم إرسال الجلسة إلى الرسائل المحفوظة")
 def LOADVARS():
     config_file = "var.json"
     if os.path.exists(config_file):
