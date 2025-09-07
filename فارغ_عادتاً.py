2e677b49d97be016519e6488deb74419019563f8
VIPABH <a891983.2@gmail.com>
2025-09-08 00:34:51 +0300

    Create فارغ_عادتاً.py

---
+++ فارغ_عادتاً.py
@@ -0,0 +1,11 @@
+from telethon import events
+from ABH import *
+@ABH.on(events.NewMessage)
+async def mx(event):
+    for i in range(385, 432):
+        x = await ABH.get_messages("x04ou", i)
+        if x:
+            msg = f'{i} موجود'
+        else:
+            msg = f'{i} غير موجود'
+    await event.reply(msg)
