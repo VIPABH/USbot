13983011bd38d53322890889c4cc19f872056ded
VIPABH <a891983.2@gmail.com>
2025-09-08 01:01:16 +0300

    Update فارغ.py

--- فارغ.py
+++ فارغ.py
@@ -1,13 +1,13 @@
-from telethon import events
-from ABH import *
-@ABH.on(events.NewMessage(pattern="^mx$", outgoing=True))
-async def mx(event):
-    await event.edit("جاري الفحص...")
-    msg = ""
-    for i in range(385, 432):
-        x = await ABH.get_messages("x04ou", ids=i)
-        if x:
-            msg += f'{i} موجود\n'
-        else:
-            msg += f'{i} غير موجود\n'
-    await event.edit(msg)
+# from telethon import events
+# from ABH import *
+# @ABH.on(events.NewMessage(pattern="^mx$", outgoing=True))
+# async def mx(event):
+#     await event.edit("جاري الفحص...")
+#     msg = ""
+#     for i in range(385, 432):
+#         x = await ABH.get_messages("x04ou", ids=i)
+#         if x:
+#             msg += f'{i} موجود\n'
+#         else:
+#             msg += f'{i} غير موجود\n'
+#     await event.edit(msg)
