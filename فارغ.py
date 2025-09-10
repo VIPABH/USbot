cc5a7883c7394966d466b3eebfca9a234b17f7d9
VIPABH <a891983.2@gmail.com>
2025-09-10 14:36:22 +0300

    Update فارغ.py

--- فارغ.py
+++ فارغ.py
@@ -1,13 +1,18 @@
-# from telethon import events
-# from ABH import *
-# @ABH.on(events.NewMessage(pattern="^mx$", outgoing=True))
-# async def mx(event):
-#     await event.edit("جاري الفحص...")
-#     msg = ""
-#     for i in range(385, 432):
-#         x = await ABH.get_messages("x04ou", ids=i)
-#         if x:
-#             msg += f'{i} موجود\n'
-#         else:
-#             msg += f'{i} غير موجود\n'
-#     await event.edit(msg)
+from telethon import events
+from ABH import *
+@ABH.on(events.NewMessage(pattern="^صيد (.+)$", outgoing=True))
+async def save(e):
+    user = e.pattern_match.group(1)
+    if not user or not user.startswith('@'):
+        await e.edit('عذرا بس الامر يعمل ك صيد @wfffp')
+        return
+    r.set(f"صيد:{e.sender_id}", user)
+    await e.reply(f'تم تخزين {user} للصيد')
+@ABH.on(events.NewMessage)
+async def h(e):
+    x = r.get(f"صيد:{e.sender_id}")
+    if not x:
+        return
+    z = await ABH.get_entity(x)
+    if not z:
+        await ABH.send_message('me', f'اليوزر متاح {x}')
