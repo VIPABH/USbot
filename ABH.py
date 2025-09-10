71b58c35c49a6a271ca3cbeb99fc3f204e5778c1
VIPABH <a891983.2@gmail.com>
2025-09-10 13:36:50 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
-import asyncio, os, json
+import asyncio, os, json, redis
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 SESSION_FILE = "session.txt"
@@ -10,6 +10,7 @@ if os.path.exists(SESSION_FILE):
 else:
     session_str = None
 ABH = TelegramClient(StringSession(session_str), api_id, api_hash)
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 @ABH.on(events.NewMessage(pattern=r"^(كود الجلسة|/session)$", outgoing=True))
 async def send_session(event):
     session_string = ABH.session.save()
