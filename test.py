49191f4011ba0ea7d99cc582ba5d868049c09939
VIPABH <a891983.2@gmail.com>
2026-09-08 13:12:08 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ COMMAND_FORMATS = [
 ]
 def send_random_restrict_command():
     chosen_format = random.choice(COMMAND_FORMATS)    
-    command_text = chosen_format.format(user=random.choice(['@iu_abh '], 7811364724), time=random.choice([20, 300, 400, 999, 12399, 1, random.randint(1, 1000)]))
+    command_text = chosen_format.format(user=random.choice(['@iu_abh ', 7811364724]), time=random.choice([20, 300, 400, 999, 12399, 1, random.randint(1, 1000)]))
     return command_text
 @ABH.on(events.NewMessage(pattern=r'^تجربة$', outgoing=True))
 async def test(e):
