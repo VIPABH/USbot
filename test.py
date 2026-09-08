2a6cebd6861fbff19321139e91a8aa5ab9827143
VIPABH <a891983.2@gmail.com>
2026-09-08 12:44:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ COMMAND_FORMATS = [
     "تقييد عام {time} {user}",
     "تقييد عام {user} {time}"  
 ]
-async def send_random_restrict_command():
+def send_random_restrict_command():
     chosen_format = random.choice(COMMAND_FORMATS)    
     command_text = chosen_format.format(user='@iu_abh ', time=random.choice([20, 300, 400, 999, 12399, 1]))
     return command_text
