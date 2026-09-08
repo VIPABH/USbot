b07ae8fa9cadfd3b715addc65b5782ce0a6b919d
VIPABH <a891983.2@gmail.com>
2026-09-08 12:37:55 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+from Resources import *
+import random, asyncio
+from ABH import *
+COMMAND_FORMATS = [
+    "تقييد عام {time} {user}",
+    "تقييد عام {user} {time}"  
+]
+async def send_random_restrict_command():
+    chosen_format = random.choice(COMMAND_FORMATS)    
+    command_text = chosen_format.format(user='@iu_abh ', time=random.choice([20, 300, 400, 999, 12399, 1]))
+    return command_text
+@ABH.on(events.NewMessage(pattern=r'^تجربة$', outgoing=True))
+async def test(e):
+    for _ in range(25):
+        text = send_random_restrict_command()
+        await e.respond(text)
+        await asyncio.sleep(0.5)
