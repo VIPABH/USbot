009728780c0f8be9cd4487ba5503f7831759a045
VIPABH <a891983.2@gmail.com>
2026-09-13 21:39:00 +0300

    Update انشاء_مجموعات.py

--- انشاء_مجموعات.py
+++ انشاء_مجموعات.py
@@ -100,7 +100,6 @@ async def create_special_channels_and_groups(current_day, current_month, date_st
 
 @ABH.on(events.NewMessage(outgoing=True))
 async def check_on_outgoing_message(event):
-    print("تم استلام الحدث")
     now = datetime.now()
     current_day = now.day
     current_month = now.month
@@ -114,5 +113,5 @@ async def check_on_outgoing_message(event):
 
     if date_str in created_dates:
         return
-
+    print("تم استلام الحدث")
     await create_special_channels_and_groups(current_day, current_month, date_str)
